2a56eac90b0209ca3eed359588823e7a3d0d1b05
Joshua Markle <joshuamarkle25@gmail.com>
2023-04-23 03:58:37 -0400

    Complete homepage animations 	modified:   public/css/style.css     modified:   public/index.html 	modified:   public/scripts/animations.js 	modified:   public/test.html

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,32 +1,71 @@
-// Animate Title
-document.addEventListener("DOMContentLoaded", function() {
-    fadeInTitle();
-});
+// Helpful Viewport Function
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    const val = rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+    console.log(val);
+    return val;
+}
 
+// Animate Title
+let titleFadedIn = false;
 function fadeInTitle() {
     let title = document.getElementById("title");
     setTimeout(function() {
         title.classList.add("fade-in", "show");
+        gsap.to(title, { y: 0, opacity: 1, delay: 0 });
     }, 250);
+    titleFadedIn = true;
 }
 
-
+function fadeOutTitle() {
+    let title = document.getElementById("title");
+    gsap.to(title, {
+      y: -50,
+      opacity: 0,
+      duration: 1,
+      ease: "power2.out"
+    });
+    titleFadedIn = false;
+  }
 
 // Animate Elements
-function fadeInElement(name) {
+function fadeInElement(name, delay) {
     let title = document.getElementById(name);
     let titlePosition = title.getBoundingClientRect().top;
     let screenPosition = window.innerHeight;
     if (titlePosition < screenPosition) {
         setTimeout(function() {
             title.classList.add("fade-in", "show");
-        }, 250);
+            gsap.to(title, { opacity: 1, y: 0 });
+        }, delay);
     }
 }
 
-window.addEventListener("scroll", function() {fadeInElement("projects");});
+document.addEventListener("DOMContentLoaded", function() {
+    fadeInTitle();
+    fadeInElement("title-image", 200);
+    fadeInElement("nav-1", 100);
+    fadeInElement("nav-2", 200);
+    fadeInElement("nav-3", 300);
+    fadeInElement("nav-4", 400);
+    fadeInElement("nav-5", 200);
+    fadeInElement("nav-6", 100);
+});
 
+window.addEventListener("scroll", function() {
+    let title = document.getElementById("title");
+    if (isInViewport(title)) {
+        if (!titleFadedIn) {
+            fadeInTitle();
+        }
+    } else {
+        if (titleFadedIn) {
+            fadeOutTitle();
+        }
+    }
+});
 
+window.addEventListener("scroll", function() {fadeInElement("projects", 250);});
 
 // Animate Containers
 function animateContainers() {
@@ -35,8 +74,10 @@ function animateContainers() {
         const containerPosition = container.getBoundingClientRect().top;
         const screenPosition = window.innerHeight;
         if (containerPosition < screenPosition) {
-            gsap.to(container, { opacity: 1, y: 0});
-            container.classList.add('animate');
+            setTimeout(function() {
+                gsap.to(container, { opacity: 1, y: 0 });
+                container.classList.add('animate');
+            }, 500);
         }
     });
 }
@@ -46,21 +87,3 @@ window.addEventListener('scroll', () => {
 });
 
 const container = document.querySelector('.container');
-
-// Initialize the tween to set the initial scale
-gsap.set(container, { scale: 1 });
-
-// Create the tween to animate the scale on hover
-const hoverTween = gsap.to(container, {
-  scale: 1.2,
-  duration: 0.3,
-});
-
-// Add the mouseenter and mouseleave events
-container.addEventListener('mouseenter', () => {
-  hoverTween.play();
-});
-
-container.addEventListener('mouseleave', () => {
-  hoverTween.reverse();
-});
