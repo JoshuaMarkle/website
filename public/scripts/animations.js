c7638a34d7ff050d709abd0849c72fa1ea53853b
Joshua Markle <joshuamarkle25@gmail.com>
2023-04-23 01:50:41 -0400

    Added basic animations 	modified:   public/css/style.css 	modified:   public/index.html 	modified:   public/scripts/animations.js 	modified:   public/test.html

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,23 +1,66 @@
+// Animate Title
+document.addEventListener("DOMContentLoaded", function() {
+    fadeInTitle();
+});
+
 function fadeInTitle() {
     let title = document.getElementById("title");
-    let titlePosition = title.offsetTop;
+    setTimeout(function() {
+        title.classList.add("fade-in", "show");
+    }, 250);
+}
+
+
+
+// Animate Elements
+function fadeInElement(name) {
+    let title = document.getElementById(name);
+    let titlePosition = title.getBoundingClientRect().top;
     let screenPosition = window.innerHeight;
     if (titlePosition < screenPosition) {
         setTimeout(function() {
             title.classList.add("fade-in", "show");
-        }, 500)
+        }, 250);
     }
 }
 
-function fadeInProjects() {
-    let title = document.getElementById("projects");
-    let titlePosition = title.offsetTop;
-    let screenPosition = window.innerHeight;
-    if (titlePosition < screenPosition) {
-        setTimeout(function() {
-            title.classList.add("fade-in", "show");
-        }, 500)
-    }
+window.addEventListener("scroll", function() {fadeInElement("projects");});
+
+
+
+// Animate Containers
+function animateContainers() {
+    const containers = document.querySelectorAll('.container');
+    containers.forEach((container) => {
+        const containerPosition = container.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight;
+        if (containerPosition < screenPosition) {
+            gsap.to(container, { opacity: 1, y: 0});
+            container.classList.add('animate');
+        }
+    });
 }
 
-window.addEventListener("scroll", fadeInTitle);
+window.addEventListener('scroll', () => {
+    animateContainers();
+});
+
+const container = document.querySelector('.container');
+
+// Initialize the tween to set the initial scale
+gsap.set(container, { scale: 1 });
+
+// Create the tween to animate the scale on hover
+const hoverTween = gsap.to(container, {
+  scale: 1.2,
+  duration: 0.3,
+});
+
+// Add the mouseenter and mouseleave events
+container.addEventListener('mouseenter', () => {
+  hoverTween.play();
+});
+
+container.addEventListener('mouseleave', () => {
+  hoverTween.reverse();
+});
