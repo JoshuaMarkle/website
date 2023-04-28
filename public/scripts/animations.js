638bc5e240ca40f3efe2cb0ba36f17f69819d0f1
Joshua Markle <joshuamarkle25@gmail.com>
2023-04-28 18:32:37 -0400

    Update animations 	modified:   public/css/style.css 	modified:   public/index.html 	modified:   public/scripts/animations.js

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,71 +1,42 @@
-// Helpful Viewport Function
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    const val = rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight);
-    console.log(val);
-    return val;
-}
-
-// Animate Title
-let titleFadedIn = false;
-function fadeInTitle() {
-    let title = document.getElementById("title");
-    setTimeout(function() {
-        title.classList.add("fade-in", "show");
-        gsap.to(title, { y: 0, opacity: 1, delay: 0 });
-    }, 250);
-    titleFadedIn = true;
-}
-
-function fadeOutTitle() {
-    let title = document.getElementById("title");
-    gsap.to(title, {
-      y: -50,
-      opacity: 0,
-      duration: 1,
-      ease: "power2.out"
-    });
-    titleFadedIn = false;
-  }
-
 // Animate Elements
-function fadeInElement(name, delay) {
-    let title = document.getElementById(name);
+function fadeInElement(name, single) {
+    let title = document.getElementById(name + "-title");
+    let content = document.getElementById(name + "-content")
+    if (single) {
+        title = document.getElementById(name);
+    }
     let titlePosition = title.getBoundingClientRect().top;
     let screenPosition = window.innerHeight;
-    if (titlePosition < screenPosition) {
+    if (titlePosition < screenPosition - 200) {
         setTimeout(function() {
             title.classList.add("fade-in", "show");
             gsap.to(title, { opacity: 1, y: 0 });
-        }, delay);
+        }, 250);
+        if (!single) {
+            setTimeout(function() {
+                title.classList.add("fade-in", "show");
+                gsap.to(content, { opacity: 1, y: 0 });
+            }, 350);
+        }
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    fadeInTitle();
-    fadeInElement("title-image", 200);
-    fadeInElement("nav-1", 100);
-    fadeInElement("nav-2", 200);
-    fadeInElement("nav-3", 300);
-    fadeInElement("nav-4", 400);
-    fadeInElement("nav-5", 200);
-    fadeInElement("nav-6", 100);
+    // fadeInTitle();
+    fadeInElement("title", true)
+    fadeInElement("title-image", true);
+    fadeInElement("nav-1", true);
+    fadeInElement("nav-2", true);
+    fadeInElement("nav-3", true);
+    fadeInElement("nav-4", true);
+    fadeInElement("nav-5", true);
+    fadeInElement("nav-6", true);
 });
 
-window.addEventListener("scroll", function() {
-    let title = document.getElementById("title");
-    if (isInViewport(title)) {
-        if (!titleFadedIn) {
-            fadeInTitle();
-        }
-    } else {
-        if (titleFadedIn) {
-            fadeOutTitle();
-        }
-    }
-});
-
-window.addEventListener("scroll", function() {fadeInElement("projects", 250);});
+window.addEventListener("scroll", function() {fadeInElement("projects", true);});
+window.addEventListener("scroll", function() {fadeInElement("about", false);});
+window.addEventListener("scroll", function() {fadeInElement("skills", false);});
+window.addEventListener("scroll", function() {fadeInElement("containers", false);});
 
 // Animate Containers
 function animateContainers() {
@@ -73,7 +44,7 @@ function animateContainers() {
     containers.forEach((container) => {
         const containerPosition = container.getBoundingClientRect().top;
         const screenPosition = window.innerHeight;
-        if (containerPosition < screenPosition) {
+        if (containerPosition < screenPosition - 100) {
             setTimeout(function() {
                 gsap.to(container, { opacity: 1, y: 0 });
                 container.classList.add('animate');
