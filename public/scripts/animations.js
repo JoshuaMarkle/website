e35ccdd078d3f78cad94a744ecee9d8bfaf15822
Joshua Markle <joshuamarkle25@gmail.com>
2023-04-30 02:02:42 -0400

    new file:   public/projects/template.html

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -1,36 +1,54 @@
 // Animate Elements
-function fadeInElement(name, single) {
+function fadeInElement(name, single, customy = 0) {
     let title = document.getElementById(name + "-title");
     let content = document.getElementById(name + "-content")
     if (single) {
         title = document.getElementById(name);
     }
+    if (title == null) {
+        return;
+    }
     let titlePosition = title.getBoundingClientRect().top;
     let screenPosition = window.innerHeight;
-    if (titlePosition < screenPosition - 200) {
+    if (titlePosition < screenPosition - 150) {
         setTimeout(function() {
             title.classList.add("fade-in", "show");
-            gsap.to(title, { opacity: 1, y: 0 });
+            gsap.to(title, { opacity: 1, y: customy });
         }, 250);
         if (!single) {
             setTimeout(function() {
                 title.classList.add("fade-in", "show");
-                gsap.to(content, { opacity: 1, y: 0 });
+                gsap.to(content, { opacity: 1, y: customy });
             }, 350);
         }
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    // fadeInTitle();
     fadeInElement("title", true)
     fadeInElement("title-image", true);
+    fadeInElement("banner", true, 250);
+
     fadeInElement("nav-1", true);
     fadeInElement("nav-2", true);
     fadeInElement("nav-3", true);
     fadeInElement("nav-4", true);
     fadeInElement("nav-5", true);
     fadeInElement("nav-6", true);
+
+    for (let i = 0; i < 20; i++) {
+        if (document.getElementById("content" + i) != null) {
+            let title = document.getElementById("content" + i);
+            let titlePosition = title.getBoundingClientRect().top;
+            let screenPosition = window.innerHeight;
+            if (titlePosition < screenPosition - 200) {
+                console.log("fadded early: content" + i);
+                fadeInElement("content" + i, false);
+            } else {
+                window.addEventListener("scroll", function() {fadeInElement("content" + i, false);});
+            }
+        }
+    }
 });
 
 window.addEventListener("scroll", function() {fadeInElement("projects", true);});
