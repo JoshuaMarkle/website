e053dd0ba0605febd08727634dd56afc174fa277
JoshuaMarkle <joshuamarkle25@gmail.com>
2024-10-08 16:38:30 -0400

    Added a links page and fixed the goofy background

--- public/scripts/animations.js
+++ public/scripts/animations.js
@@ -6,4 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
 		event.preventDefault();
 		aboutSection.classList.toggle('expand');
 	});
+
+	// Fade in cards
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry, index) => {
+			if (entry.isIntersecting) {
+				let delay = index * 100;
+				entry.target.style.transitionDelay = `${delay}ms`;
+				entry.target.classList.add('fade-in');
+				observer.unobserve(entry.target);
+			}
+		});
+	}, { threshold: 0.1 });
+
+	const cards = document.querySelectorAll('.card');
+	cards.forEach((card, index) => observer.observe(card));
 });
