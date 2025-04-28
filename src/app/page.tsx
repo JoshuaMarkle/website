c98535a91e8a739ba280995574ffb0f599377ea9
Joshua Markle <joshuamarkle25@gmail.com>
2025-04-27 21:21:20 -0400

    Update marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,17 @@ import MarqueeSine from "./components/Marquee";
 export default function Home() {
 	return (
 		<main>
-			<div className="justify-center p-8 mx-0 md:mx-32 xl:mx-64 mt-16">
+			<div className="p-8 mx-0 md:mx-32 xl:mx-64 mt-16">
 				<div>
 					<h1 className="text-8xl">Hi</h1>
 					<h2 className="text-8xl">I'm Josh</h2>
-				</div>
-				<div className="mt-16">
-					<p className="text-4xl">“I know of no better life purpose than to perish in attempting the great and the impossible.”</p>
+					<p className="text-4xl mt-16">CS Student @ University of Virginia</p>
 				</div>
 			</div>
 			<MarqueeSine/>
+			<div className="p-8 mx-0 md:mx-32 xl:mx-64 mt-16">
+				<p className="text-4xl">“I know of no better life purpose than to perish in attempting the great and the impossible.”</p>
+			</div>
 		</main>
 	);
 }
