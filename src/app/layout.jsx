6363b5adb14d61069e68e8c4d97bf5c669295690
JoshuaMarkle <joshuamarkle25@gmail.com>
2025-07-19 15:48:19 -0400

    Added favicons and <head>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,33 +5,48 @@ import "@/app/globals.css";
 
 export const metadata = {
   title: "Joshua Markle | Portfolio",
-  description: "Hi, I'm Josh. Check out my stuff",
-  // openGraph: {
-  //   url: "https://algotype.net",
-  //   title: "AlgoType | Typing Practice For Programmers",
-  //   description:
-  //     "AlgoType.net is designed to help programmers train their typing skills on syntax and special characters in order to achive flow and maximize productivity.",
-  //   images: [
-  //     {
-  //       url: "https://algotype.net/twitter-card.png",
-  //       width: 1200,
-  //       height: 630,
-  //       alt: "AlgoType Logo",
-  //     },
-  //   ],
-  // },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "AlgoType | Typing Practice For Programmers",
-  //   description:
-  //     "AlgoType.net is designed to help programmers train their typing skills on syntax and special characters in order to achive flow and maximize productivity.",
-  //   images: ["https://algotype.net/twitter-card.png"],
-  // },
+  description: "Hi, I'm Josh. Check out my stuff!",
+  openGraph: {
+    url: "https://joshuamarkle.com",
+    title: "Joshua Markle | Portfolio",
+    description: "Hi, I'm Josh. Check out my stuff!",
+    images: [
+      {
+        url: "https://joshuamarkle.com/images/twitter-card.png",
+        width: 1200,
+        height: 630,
+        alt: "Joshua Markle Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Joshua Markle | Portfolio",
+    description: "Hi, I'm Josh. Check out my stuff!",
+    images: ["https://joshuamarkle/images/twitter-card.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${GeistSans.className} ${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
